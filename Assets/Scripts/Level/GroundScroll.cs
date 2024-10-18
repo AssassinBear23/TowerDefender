d0aef678e75330d689aef02aeed861c7bd32e0e5
Beerent Huizer <566463@student.saxion.nl>
2024-10-18 21:16:20 +0200

    Adding next platform has been slightly overhauled.
    Now possible to make different size platforms as long as you give the extent of it in the GroundScroll script of the new prefab.
    Also added MovePlatform() to Start event, due to gaps being created at platform creation

--- Assets/Scripts/Level/GroundScroll.cs
+++ Assets/Scripts/Level/GroundScroll.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GroundScroll : MonoBehaviour
@@ -5,13 +6,22 @@ public class GroundScroll : MonoBehaviour
     [Header("Deletion values")]
     [Tooltip("The distance from the player that the platform will be deleted")]
     [SerializeField] private float deleteDistance = 10f;
+    [Tooltip("The midpoint of the platform")]
     [SerializeField] private Transform midPointTransform;
+    [Tooltip("The start point of the platform")]
+    [Min(0)] public float offset;
+
+
+    void Start()
+    {
+        MoveGround();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        MoveGround();
         DestroyAfterPass();
+        MoveGround();   
     }
 
     void DestroyAfterPass()
@@ -22,7 +32,7 @@ public class GroundScroll : MonoBehaviour
             //    "\nGameManager instance:\t" + GameManager.instance +
             //    "\ngroundManager reference in instance:\t" + GameManager.instance.groundManager +
             //    "\ngameObject reference passed in Broadcast method:\t" + gameObject);
-            GameManager.instance.groundManager.BroadcastMessage("PlatformDestroy", gameObject);
+            GameManager.instance.groundManager.PlatformDestroy(gameObject);
         }
     }
 
