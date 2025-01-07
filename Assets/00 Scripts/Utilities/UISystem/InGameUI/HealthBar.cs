1821fe434552990237999318d2b1aec0f6959e57
Beerent Huizer <566463@student.saxion.nl>
2025-01-07 16:58:44 +0100

    Fixed health not showing for Tower

--- Assets/00 Scripts/Utilities/UISystem/InGameUI/HealthBar.cs
+++ Assets/00 Scripts/Utilities/UISystem/InGameUI/HealthBar.cs
@@ -22,19 +22,29 @@ public class HealthBar : MonoBehaviour
     {
         _slider.maxValue = _healthController.MaxHealth;
         SetColor();
+        CheckForFillVisibility();
     }
 
     public void UpdateHealth()
     {
         _slider.value = _healthController.CurrentHealth;
+        Debug.Log($"{gameObject.name}"
+                  + $"\nHealth has been updated to:\t{_slider.value}"
+                  + $"\nMax health is:\t\t{_slider.maxValue}"
+                  + $"\nAre they considered equal to each other? {_slider.value == _slider.maxValue}");
         SetColor();
+        CheckForFillVisibility();
+    }
+
+    private void CheckForFillVisibility()
+    {
         if (_slider.value == _slider.maxValue)
         {
             _fill.enabled = false;
         }
         else
         {
-             _fill.enabled = true;
+            _fill.enabled = true;
         }
     }
 
